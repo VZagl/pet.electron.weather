544f3ee2aee66781bccb87de2a58b5eca5c7ecd5
Zahliada VM <waah777@gmail.com>
2025-04-01 16:25:35 +0300

    добавление: использование настроек приложения из контекста в компоненте WeatherForecast

--- src/renderer/windows/main/features/WeatherForecast.tsx
+++ src/renderer/windows/main/features/WeatherForecast.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { useAppConfig_Store } from '~/stores/AppConfig_StoreContext';
+import './WeatherForecast.scss';
 
-export const WeatherForecast: React.FC = () => {
+export const WeatherForecast = observer(() => {
+	const store = useAppConfig_Store();
+
 	return (
 		<div className='weatherForecast'>
 			<h2>Прогноз погоды</h2>
 			<div className='spinner' />
 			{/* Добавьте здесь логику для отображения прогноза погоды */}
+			<div className='text'>
+				cities = <span>{store?.config.cities}</span>
+			</div>
 		</div>
 	);
-};
+});
